99a85e2ee1f1afe7763477f372e0e0b51bbb1503
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-10-20 10:39:51 +0100

    Navbar

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,18 @@
+import Header from '../../components/Header'
+import { Box } from '@mui/system'
+
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <Box m={'20px'}>
+      <Box
+        display={'flex'}
+        justifyContent="space-between"
+        alignItems={'center'}
+      >
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  )
 }
 
 export default Dashboard
