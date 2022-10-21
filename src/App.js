5e61bd6d6089236ad7030b1a9c5c6dd0d0b68ffa
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-10-21 12:23:21 +0100

    Tables with Mui Data Grid (contacts, invoices, team)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { Route, Routes } from 'react-router-dom'
 import Topbar from './scenes/global/Topbar'
 import Dashboard from './scenes/dashboard'
 import Sidebar from './scenes/global/Sidebar'
-//import Team from './scenes/team'
-//import Invoices from './scenes/invoices'
-//import Contacts from './scenes/contacts'
+import Team from './scenes/team'
+import Invoices from './scenes/invoices'
+import Contacts from './scenes/contacts'
 //import Bar from './scenes/bar'
 //import Form from './scenes/form'
 //import Line from './scenes/line'
@@ -27,9 +27,9 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path='/team' element={<Team/>}/> */}
-              {/* <Route path='/contacts' element={<Contacts/>}/> */}
-              {/* <Route path='/invoices' element={<Invoices/>}/> */}
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
               {/* <Route path='/form' element={<Form/>}/> */}
               {/* <Route path='/bar' element={<Bar/>}/> */}
               {/* <Route path='/pie' element={<Pie/>}/> */}
