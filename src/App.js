5270ff3fc8f88426f525336b1b71c373140a387d
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-10-21 22:20:38 +0100

    Profile Form with Formik and Yup

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Team from './scenes/team'
 import Invoices from './scenes/invoices'
 import Contacts from './scenes/contacts'
 //import Bar from './scenes/bar'
-//import Form from './scenes/form'
+import Form from './scenes/form'
 //import Line from './scenes/line'
 //import Pie from './scenes/pie'
 //import FAQ from './scenes/faq'
@@ -30,7 +30,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              {/* <Route path='/form' element={<Form/>}/> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path='/bar' element={<Bar/>}/> */}
               {/* <Route path='/pie' element={<Pie/>}/> */}
               {/* <Route path='/line' element={<Line/>}/> */}
