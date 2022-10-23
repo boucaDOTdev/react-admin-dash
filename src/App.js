e6f11780cac9915280328c12744deab4d7345890
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-10-23 17:15:13 +0100

    Calendar using FullCalendar Package

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from './scenes/form'
 //import Pie from './scenes/pie'
 //import FAQ from './scenes/faq'
 //import Geography from './scenes/geography'
-//import Calendar from './scenes/calendar'
+import Calendar from './scenes/calendar'
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -21,22 +21,22 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app">
+        <div className='app'>
           <Sidebar />
-          <main className="content">
+          <main className='content'>
             <Topbar />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
               {/* <Route path='/bar' element={<Bar/>}/> */}
               {/* <Route path='/pie' element={<Pie/>}/> */}
               {/* <Route path='/line' element={<Line/>}/> */}
               {/* <Route path='/faq' element={<FAQ/>}/> */}
               {/* <Route path='/geography' element={<Geography/>}/> */}
-              {/* <Route path='/calendar' element={<Calendar/>}/> */}
+              <Route path='/calendar' element={<Calendar />} />
             </Routes>
           </main>
         </div>
