afd49db25821958a9bd19c2dc9943658cc2ea176
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-11-21 20:09:39 +0000

    Bar Chart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sidebar from './scenes/global/Sidebar'
 import Team from './scenes/team'
 import Invoices from './scenes/invoices'
 import Contacts from './scenes/contacts'
-//import Bar from './scenes/bar'
+import Bar from './scenes/bar'
 import Form from './scenes/form'
 //import Line from './scenes/line'
 //import Pie from './scenes/pie'
@@ -31,7 +31,7 @@ function App() {
               <Route path='/contacts' element={<Contacts />} />
               <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
-              {/* <Route path='/bar' element={<Bar/>}/> */}
+              <Route path='/bar' element={<Bar />} />
               {/* <Route path='/pie' element={<Pie/>}/> */}
               {/* <Route path='/line' element={<Line/>}/> */}
               <Route path='/faq' element={<FAQ />} />
