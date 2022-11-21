da37c8344b17d709ea109f3f5529fdde55ac0c90
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-11-21 20:42:48 +0000

    Pie chart

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contacts from './scenes/contacts'
 import Bar from './scenes/bar'
 import Form from './scenes/form'
 //import Line from './scenes/line'
-//import Pie from './scenes/pie'
+import Pie from './scenes/pie'
 import FAQ from './scenes/faq'
 //import Geography from './scenes/geography'
 import Calendar from './scenes/calendar'
@@ -32,7 +32,7 @@ function App() {
               <Route path='/invoices' element={<Invoices />} />
               <Route path='/form' element={<Form />} />
               <Route path='/bar' element={<Bar />} />
-              {/* <Route path='/pie' element={<Pie/>}/> */}
+              <Route path='/pie' element={<Pie />} />
               {/* <Route path='/line' element={<Line/>}/> */}
               <Route path='/faq' element={<FAQ />} />
               {/* <Route path='/geography' element={<Geography/>}/> */}
