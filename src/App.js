0d82e9fcfdb58b626b75554460734df51729a4d1
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2022-11-23 11:28:21 +0000

    Line Chart and Geography Chart

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Invoices from './scenes/invoices'
 import Contacts from './scenes/contacts'
 import Bar from './scenes/bar'
 import Form from './scenes/form'
-//import Line from './scenes/line'
+import Line from './scenes/line'
 import Pie from './scenes/pie'
 import FAQ from './scenes/faq'
-//import Geography from './scenes/geography'
+import Geography from './scenes/geography'
 import Calendar from './scenes/calendar'
 
 function App() {
@@ -33,9 +33,9 @@ function App() {
               <Route path='/form' element={<Form />} />
               <Route path='/bar' element={<Bar />} />
               <Route path='/pie' element={<Pie />} />
-              {/* <Route path='/line' element={<Line/>}/> */}
+              <Route path='/line' element={<Line />} />
               <Route path='/faq' element={<FAQ />} />
-              {/* <Route path='/geography' element={<Geography/>}/> */}
+              <Route path='/geography' element={<Geography />} />
               <Route path='/calendar' element={<Calendar />} />
             </Routes>
           </main>
